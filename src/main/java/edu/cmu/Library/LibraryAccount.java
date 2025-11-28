fd327c5a3d59100f925797341ed2032872f2d78c
Bruno Gracia <bgracia@uaudio.com>
2025-11-27 21:41:16 -0500

    Fix Quality design principle violation in LibraryAccount

--- src/main/java/edu/cmu/Library/LibraryAccount.java
+++ src/main/java/edu/cmu/Library/LibraryAccount.java
@@ -1,21 +1,38 @@
 package edu.cmu.Library;
 
+class EmptyBookListException extends Exception {
+    public EmptyBookListException(String message) {
+        super(message);
+    }
+}
+
 public class LibraryAccount {
     private LibraryService libraryService;
  
+    // BG: Params are complicated (requires formatting of userId)
+    // BG: For no books, an empty array is returned instead of null or an exception
+
+    // BG: Violates Quality API design principle (Q3) since
+    // BG: client code needs to know the userId format and handle empty arrays.
+
+
     /**
      * Retrieves an array of checked out books associated with the specified user ID. If the user
-     * has no books checked out, the returned list will be empty. Since multiple households may
-     * share a single account, the user ID is of the form "libraryID:userName".
-     * e.g., "12345:John Doe"
+     * has no books checked out, the returned list will be empty. 
      *
-     * @param userId the ID of the user whose books are to be retrieved
+     * @param libraryId the ID of the library
+     * @param userName the name of the user whose books are to be retrieved
      * @return an array of Book objects the user has checked out
-     */
-    public Book[] getBooks(String userId) {
-        String[] parts = userId.split(":");
-        String name = parts[0];
-        String id = parts[1];
-        return libraryService.getBooks(name, id);        
+     * @throws EmptyBookListException if the user has no books checked out
+     */    
+    public Book[] getBooks(String libraryId, String userName) throws EmptyBookListException {
+
+        Book[] books = libraryService.getBooks(libraryId, userName);
+        
+        if (books.length == 0) {
+            throw new EmptyBookListException("No books checked out for user: " + userName);
+        }
+
+        return books;
     }
 }
